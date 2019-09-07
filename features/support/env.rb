46fa078324c46dc812e5c1e536e3f34524a9c36c
Thialison Souza <thialison.souza@hotmail.com>
2019-09-07 14:50:37 -0300

    Update to use with container Docker

--- features/support/env.rb
+++ features/support/env.rb
@@ -11,8 +11,26 @@ require 'faker'
 World(Pages)
 
 Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+	capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+	  chromeOptions: { args: %w(disable-infobars --disable-dev-shm-usage --no-sandbox window-size=1920,1080) })
+	Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
 end
+
+Capybara.default_driver = :selenium
+Capybara.default_max_wait_time = 15
+
+require_relative 'helper_page.rb'
+require 'rspec'
+require 'cucumber'
+require 'selenium-webdriver'
+require 'capybara'
+require 'capybara/cucumber'
+require 'pry-byebug'
+require 'site_prism'
+require 'faker'
+
+World(Pages)
+
 
 Capybara.default_driver = :selenium
 Capybara.default_max_wait_time = 15
