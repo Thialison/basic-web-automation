81ef70b340ab38a38044a6ca4eac0c601931bbbf
Thialison Souza <thialison.souza@hotmail.com>
2019-09-07 14:50:57 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -18,19 +18,3 @@ end
 
 Capybara.default_driver = :selenium
 Capybara.default_max_wait_time = 15
-
-require_relative 'helper_page.rb'
-require 'rspec'
-require 'cucumber'
-require 'selenium-webdriver'
-require 'capybara'
-require 'capybara/cucumber'
-require 'pry-byebug'
-require 'site_prism'
-require 'faker'
-
-World(Pages)
-
-
-Capybara.default_driver = :selenium
-Capybara.default_max_wait_time = 15
